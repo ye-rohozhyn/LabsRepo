976f70056972c2fd0f53cb09fbab93f7321ae632
dev-yevhen <yerohozhyn@gmail.com>
2023-08-26 16:16:32 +0300

    Added function for calculation fibonacci numbers

--- Lab1Project/main.py
+++ Lab1Project/main.py
@@ -1,4 +1,5 @@
 from prime_numbers import *
+from fibonacci import *
 
 a = int(input("Task 1\nEnter first number: "))
 b = int(input("Enter second number: "))
@@ -7,3 +8,6 @@ if a > b:
     a, b = b, a
 
 print_prime_numbers(a, b)
+
+k = int(input("Task 2\nEnter count fibonacci numbers: "))
+print(fib(k))
