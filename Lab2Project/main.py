8b77a8947739a36a25f6c9128f72ee82fd09e6d0
dev-yevhen <yerohozhyn@gmail.com>
2023-09-02 16:32:45 +0300

    Added example code

--- Lab2Project/main.py
+++ Lab2Project/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+def LangDetect(txt):
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def TransLate(txt, lang):
 
+def CodeLang(lang):
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    txt = "Доброго дня. Як справи?"
+    lang = "en"
+    print(txt)
+    print(LangDetect(txt))
+    print(TransLate(txt, lang))
+    print(CodeLang(lang))
