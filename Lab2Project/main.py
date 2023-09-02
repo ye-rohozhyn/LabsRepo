d07a12b3a4637c7469d6dd05cce5a774c05541b1
dev-yevhen <yerohozhyn@gmail.com>
2023-09-02 16:16:30 +0300

    Added translate functions

--- Lab2Project/main.py
+++ Lab2Project/main.py
@@ -1,12 +1,29 @@
+from langdetect import detect
+from translate import Translator
+
+
 def LangDetect(txt):
+    lang = detect(txt)
+    confidence = 1  # In this example, assume language detection is always accurate
+    return f"Detected(lang={lang}, confidence={confidence})"
+
 
 def TransLate(txt, lang):
+    translator = Translator(from_lang=lang, to_lang="en")
+    translated_text = translator.translate(txt)
+    return translated_text
+
 
 def CodeLang(lang):
+    # You can add a dictionary with language code to name mappings here
+    # For example, {"en": "English", "uk": "Ukrainian", ...}
+    language_names = {"en": "English", "uk": "Ukrainian"}
+    return language_names.get(lang, "Unknown")
+
 
 if __name__ == '__main__':
     txt = "Доброго дня. Як справи?"
-    lang = "en"
+    lang = "uk"
     print(txt)
     print(LangDetect(txt))
     print(TransLate(txt, lang))
