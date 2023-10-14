32347cda6168b548498e460387edfbaafe6921dc
dev-yevhen <yerohozhyn@gmail.com>
2023-10-14 18:02:43 +0300

    Added language sort

--- Lab5Project/main.py
+++ Lab5Project/main.py
@@ -14,6 +14,26 @@ def read_first_sentence(file_path):
         return None
 
 
+def process_text(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    words.sort()
+
+    ukrainian_words = [word for word in words if re.search(r'[а-яїєґіь]+', word)]
+    english_words = [word for word in words if re.search(r'[a-z]+', word)]
+
+    if ukrainian_words:
+        print("\nУкраїнські слова:")
+        print(", ".join(ukrainian_words))
+    if english_words:
+        print("\nАнглійські слова:")
+        print(", ".join(english_words))
+
+    word_count = len(words)
+    print("\nЗагальна кількість слів:", word_count)
+
+
 if __name__ == '__main__':
     file_path = 'text_file.txt'
-    read_first_sentence(file_path)
+    first_sentence = read_first_sentence(file_path)
+    if first_sentence is not None:
+        process_text(first_sentence)
